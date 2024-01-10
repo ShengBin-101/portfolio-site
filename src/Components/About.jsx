7040e3c65a847af4eabbd3a263e0a2849ddaf15b
Sheng Bin <52733750+ShengBin-101@users.noreply.github.com>
2024-01-10 13:30:26 +0000

    Fixed responsiveness for skill section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,17 +7,6 @@
 import React from "react";
 
 /**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
-
-/**
  * Sort description that expands on your title on the Home component.
  */
 const description =
@@ -27,13 +16,15 @@ const description =
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const languagesList = ["Robot Operating System (ROS)", "C/C++", "Python", "Computer Vision"];
+const languagesList = ["C/C++", "Python", "HTML/CSS/JavaScript", "Dart"];
 
 const hardwareList = ["Robot Operating System (ROS)", "Arduino"];
 
-const webList = ["HTML/CSS/JavaScript", "React"];
+const frameworkList = ["React", "Express", "Flutter", "Firebase"];
 
-const otherList = ["Git, GitHub", "Docker"];
+const databaseList = ["Firestore"];
+
+const toolList = ["Git/GitHub", "Docker", "Kubernetes"];
 
 /**
  * Use this to give more information about what you are passionate about,
@@ -45,28 +36,18 @@ const detailOrQuote =
 
 const About = () => {
   return (
-    <section className="padding responsive-section" id="about">
+    <section className="padding" id="about">
       <div className="about-content">
         <h2>About Myself</h2>
         <p className="large">{description}</p>
         <p className="large">{detailOrQuote}</p>
-        <br />
-
       </div>
       <div className="skill-content">
         <h2>Skills</h2>
         <div className="skills-wrapper">
           <div className="skills-container">
-            <p className="skill-label">Programming Languages</p>
+            <p className="skill-label">Languages</p>
             {languagesList.map((skill, index) => (
-              <div key={index} className="skill-item">
-                {skill}
-              </div>
-            ))}
-          </div>
-          <div className="skills-container">
-            <p className="skill-label">Web Development</p>
-            {webList.map((skill, index) => (
               <div key={index} className="skill-item">
                 {skill}
               </div>
@@ -81,8 +62,24 @@ const About = () => {
             ))}
           </div>
           <div className="skills-container">
-            <p className="skill-label">Others</p>
-            {otherList.map((skill, index) => (
+            <p className="skill-label">Frameworks</p>
+            {frameworkList.map((skill, index) => (
+              <div key={index} className="skill-item">
+                {skill}
+              </div>
+            ))}
+          </div>
+          <div className="skills-container">
+            <p className="skill-label">Database</p>
+            {databaseList.map((skill, index) => (
+              <div key={index} className="skill-item">
+                {skill}
+              </div>
+            ))}
+          </div>
+          <div className="skills-container">
+            <p className="skill-label">Tools</p>
+            {toolList.map((skill, index) => (
               <div key={index} className="skill-item">
                 {skill}
               </div>
